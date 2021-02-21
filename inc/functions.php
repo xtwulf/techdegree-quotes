0425f9d7c63a224e45bbffcc5310c201fc69c1d6
Ulf Wilke <ulf-wilke@web.de>
2021-02-21 09:01:07 +0100

    Added example files

---
+++ inc/functions.php
@@ -0,0 +1,12 @@
+// PHP - Random Quote Generator
+
+// Create the Multidimensional array of quote elements and name it quotes
+// Each inner array element should be an associative array
+
+
+
+// Create the getRandomQuuote function and name it getRandomQuote
+
+
+
+// Create the printQuote funtion and name it printQuote
