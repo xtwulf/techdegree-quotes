553a4bc3199d12b828400d82edb91ed946969fe6
Ulf Wilke <ulf-wilke@web.de>
2021-02-22 08:27:16 +0100

    V2
    Refined according Project Requirements:
    - Added additional values in Array
    - BG color is random now

--- inc/functions.php
+++ inc/functions.php
@@ -2,31 +2,52 @@
 
 // PHP - Random Quote Generator
 
-// Read JSON file
-
-// Create the Multidimensional array of quote elements and name it quotes
-// Each inner array element should be an associative array
-$quotes = json_decode(file_get_contents('inc/quotes.json'), true);
 
 
-// Create the getRandomQuote function and name it getRandomQuote
+// Creating the Quotes array
+$quotes = [];
+$quotes[0] = array("quote" => "When one door closes another door opens; but we so often look so long and so regretfully upon the closed door, that we do not see the ones which open for us.", "author" => "Alexander Graham Bell", "citation" => "", "year" => "1887", "category" =>"Business");
+$quotes[1] = array("quote" => "To be a striker you need to be in good shape.", "author" => "Pele", "citation" => "", "year" => "", "category" =>"Sports");
+$quotes[2] = array("quote" => "I don't want to adult today, I just want to dog. I'll be lying down on the floor in the sun, you can pet me and bring me some snacks.", "author" => "unknown", "citation" => "", "year" => "", "category" =>"Funny");
+$quotes[3] = array("quote" => "I just don’t know if movies can ever be considered art, because there’s so much money involved… It’s all about commerce. I don’t think art can come from that place.", "author" => "Johnny Depp", "citation" => "", "year" => "", "category" =>"Celebrities");
+$quotes[4] = array("quote" => "Enjoy yourself. It's later than you think.", "author" => "unknown", "citation" => "chinese proverb", "year" => "", "category" =>"Life");
+$quotes[5] = array("quote" => "I have also seen children successfully surmounting the effects of an evil inheritance. That is due to purity being an inherent attribute of the soul.", "author" => "Mahatma Gandhi", "citation" => "quotes.net", "year" => "", "category" =>"Celebrities");
+
+
+
+// Function getRandomQuote checks the elemnts of the array and returns a random number
 
 function getRandomQuote($quotes) {
     
-    return rand(0, count($quotes['quotes'])-1);
+    return rand(0, count($quotes)-1);
 }
 
 
 
-// Create the printQuote funtion and name it printQuote
+// Function printQuote --> Print a random quote and checks, if tehr are additional elements
 
 function printQuote($quotes) {
     $number = getRandomQuote($quotes);
-    $quote = $quotes['quotes'][$number]['quote'];
-    $author = $quotes['quotes'][$number]['author'];
+    $quote = $quotes[$number]['quote'];
+    $author = $quotes[$number]['author'];
+    $citation = $quotes[$number]['citation'];
+    $year = $quotes[$number]['year'];
+    $category = $quotes[$number]['category'];
     
-    return(array('quote'=>$quote, 'author'=>$author));
-    
+    echo '
+    <p> Category: ' . $category . '</p>
+    <p class="quote"> '. $quote .' </p>
+    <p class="source"> ' . $author;
+    if ($citation != "") {
+        echo "<span class='citation'> " . $citation ." </span>";
+
+    }
+    if ($year != "") {
+        echo "<span class='year'> ".$year." </span>";
+        
+    }
+    echo "</p>";
+
 }
 
 
--- index.php
+++ index.php
@@ -6,18 +6,24 @@
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/styles.css">
+
+    
 </head>
-<body>
+<body  <?php
+   echo "style=\"background-color:".sprintf('#%06X', mt_rand(0, 0xFFFFFF)).";\"";
+  ?>>
   <div class="container">
     <div id="quote-box">
+
       <?php
       include 'inc/functions.php';
-      $quote = printQuote($quotes);
-      echo "<p class='quote'>" . $quote['quote'] . "</p>";
-      echo "<p class='source'>". $quote['author'] . "<br><span class='citation'><a href='https://gist.github.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80'>thanks to: nasrulhazim on Github</a></span></p>";
+      printQuote($quotes);
       ?>
       </div>
-    <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
+    <!-- Replaced the original on-click event and added an <a> Tag -->
+    <a href="index.php"><button id="loadQuote"  >Show another quote</button></a>
+
+    
   </div>
 </body>
 </html>
