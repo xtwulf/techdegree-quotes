0335d611739bf40308970eed4260957b1c91b040
Ulf Wilke <ulf-wilke@web.de>
2021-02-22 06:33:40 +0100

    finished version

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,33 @@
+<?php
+
 // PHP - Random Quote Generator
+
+// Read JSON file
 
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
+$quotes = json_decode(file_get_contents('inc/quotes.json'), true);
 
 
+// Create the getRandomQuote function and name it getRandomQuote
 
-// Create the getRandomQuuote function and name it getRandomQuote
+function getRandomQuote($quotes) {
+    
+    return rand(0, count($quotes['quotes'])-1);
+}
 
 
 
 // Create the printQuote funtion and name it printQuote
+
+function printQuote($quotes) {
+    $number = getRandomQuote($quotes);
+    $quote = $quotes['quotes'][$number]['quote'];
+    $author = $quotes['quotes'][$number]['author'];
+    
+    return(array('quote'=>$quote, 'author'=>$author));
+    
+}
+
+
+?>
--- index.php
+++ index.php
@@ -10,9 +10,13 @@
 <body>
   <div class="container">
     <div id="quote-box">
-      <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-      <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
-    </div>
+      <?php
+      include 'inc/functions.php';
+      $quote = printQuote($quotes);
+      echo "<p class='quote'>" . $quote['quote'] . "</p>";
+      echo "<p class='source'>". $quote['author'] . "<br><span class='citation'><a href='https://gist.github.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80'>thanks to: nasrulhazim on Github</a></span></p>";
+      ?>
+      </div>
     <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
   </div>
 </body>
